60c56ca9d08c5071154b2af1012cfd84ed9bed3c
Jashwanth-Gowda-R <jashwanth.go@gmail.com>
2023-05-08 14:00:44 +0530

    completed ui side of the app

---
+++ android/app/src/main/kotlin/com/shani/chatgpt/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.shani.chatgpt
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,32 @@
+import 'package:chatgpt/home_page.dart';
+import 'package:chatgpt/pallete.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Shani',
+      theme: ThemeData.light(useMaterial3: true).copyWith(
+        scaffoldBackgroundColor: Pallete.whiteColor,
+        appBarTheme: const AppBarTheme(
+          backgroundColor: Pallete.whiteColor,
+        ),
+        textTheme: const TextTheme(
+          bodyLarge: TextStyle(fontFamily: 'Cera Pro'),
+          bodyMedium: TextStyle(fontFamily: 'Cera Pro'),
+          bodySmall: TextStyle(fontFamily: 'Cera Pro'),
+        ),
+      ),
+      home: const HomePage(),
+    );
+  }
+}
